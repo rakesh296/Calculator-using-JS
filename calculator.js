d0ce75cbda8e64d5563a47d5642fd023b845e226
Rakesh <rksingh1452@gmail.com>
2022-10-24 12:57:07 +0530

    adding function display number in the container

--- calculator.js
+++ calculator.js
@@ -1,1 +1,41 @@
-document.getElementById('result').innerHTML=456;
+function one(){
+    val= document.getElementById('one').value;
+   document.getElementById('result').value+=val;
+}
+function two(){
+    val= document.getElementById('two').value;
+   document.getElementById('result').value+=val;
+}
+function three(){
+    val= document.getElementById('three').value;
+   document.getElementById('result').value+=val;
+}
+function four(){
+    val= document.getElementById('four').value;
+   document.getElementById('result').value+=val;
+}
+function five(){
+    val= document.getElementById('five').value;
+   document.getElementById('result').value+=val;
+}
+function six(){
+    val= document.getElementById('six').value;
+   document.getElementById('result').value+=val;
+}
+function seven(){
+    val= document.getElementById('seven').value;
+   document.getElementById('result').value+=val;
+}
+function eight(){
+    val= document.getElementById('eight').value;
+   document.getElementById('result').value+=val;
+}
+function nine(){
+    val= document.getElementById('nine').value;
+   document.getElementById('result').value+=val;
+}
+function zero(){
+    val= document.getElementById('zero').value;
+   document.getElementById('result').value+=val;
+}
+
