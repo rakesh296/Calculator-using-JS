ee5722404e7d38c012ae77234242e0c71f19c966
Rakesh <rksingh1452@gmail.com>
2022-10-27 19:41:00 +0530

    add power button to find x2,x3 of any number

--- calculator.js
+++ calculator.js
@@ -38,4 +38,70 @@ function zero(){
     val= document.getElementById('zero').value;
    document.getElementById('result').value+=val;
 }
+function dot(){
+    val= document.getElementById('dot').value;
+   document.getElementById('result').value+=val;
+}
+//opreator function:-
+
+function divide(){
+    val= document.getElementById('divide').value;
+   document.getElementById('result').value+=val;
+}
+function multiply(){
+    val= document.getElementById('multiply').value;
+   document.getElementById('result').value+=val;
+}
+function adds(){
+    val= document.getElementById('adds').value;
+   document.getElementById('result').value+=val;
+}
+
+function subs(){
+    val= document.getElementById('subs').value;
+   document.getElementById('result').value+=val;
+}
+function power(){
+    val= document.getElementById('percent').value;
+   document.getElementById('result').value+=val;
+  
+  
+
+
+}
+
+//evalute the equation using eval() function:-
+
+function equal(){
+   let x=document.getElementById('result').value;
+   let y=eval(x);
+   document.getElementById('result').value=y;
+   // console.log(x);
+   // jso=JSON.stringify(x);
+   // console.log(jso);
+   // const str=jso;
+   // const result=str.split(/[+]/);
+   // console.log(result);
+   // console.log(result.(index-1)+result.(index+1));
+   
+   
+
+}
+
+//clear  function:-
+function clr(){
+   document.getElementById('result').value=null;
+}
+
+//delete function:-
+function del(){
+ let x= document.getElementById('result').value;
+   let y=x.slice(0,-1);
+   document.getElementById('result').value=y;
+
+  }
+  
+
+
+
 
